a8a0aab24c78b55a9025dbb0c8394efdf68253a3
ayushaa4 <ayushakcayusha@gmail.com>
2026-07-12 19:54:47 +0545

    Dashain Bonus Calculator

---
+++ Week1/dashainbonus.py
@@ -0,0 +1,11 @@
+salary = int(input("Enter monthly salary: "))
+
+bonus = salary
+
+deduction = bonus * 0.10
+
+take_home_bonus = bonus - deduction
+
+print("Dashain Bonus:", bonus)
+print("Deduction:", deduction)
+print("Take Home Bonus:", take_home_bonus)
